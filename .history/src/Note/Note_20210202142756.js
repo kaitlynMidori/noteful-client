1590a607523d0b2f1db74dbeda0eba7dfde04db9
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-02-05 10:16:41 -0800

    2nd commit

---
+++ .history/src/Note/Note_20210202142756.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './Note.css'
+
+export default class Note extends React.Component {
+  static defaultProps ={
+    onDeleteNote: () => {},
+  }
+  static contextType = ApiContext;
+
+  handleClickDelete = e => {
+    e.preventDefault()
+
+    const noteId = this.props.id
+
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${config.API_KEY}`
+    },
+    })
+      .then(res => {
+        if (!res.ok) {
+            return res.then(e => Promise.reject(e))  
+        }
+        return res;
+      })
+      .then(() => {
+        this.context.deleteNote(noteId)
+        this.props.onDeleteNote(noteId)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
+
+  render() {
+    const { name, id, modified } = this.props
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`}>
+            {name}
+          </Link>
+        </h2>
+        <button
+          className='Note__delete'
+          type='button'
+          onClick={this.handleClickDelete}
+        >
+          <FontAwesomeIcon icon='trash-alt' />
+          {' '}
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {new Date(Date.parse(modified)).toString()}
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
