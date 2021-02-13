b600896683c9125faea8fe7916a68f5a2fb0dcf5
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-02-12 17:30:08 -0800

    cleared errors

---
+++ .history/src/config_20210212172248.js
@@ -0,0 +1,8 @@
+// export default {
+//     API_ENDPOINT: 'http://localhost:9090'
+//   }
+
+export default {
+  API_ENDPOINT: `https://infinite-caverns-55314.herokuapp.com/`
+  // API_KEY: process.env.REACT_APP_API_KEY,
+}
