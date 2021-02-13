e22551fdb1eb7b988bca6a0394719599df870214
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-02-12 22:18:52 -0800

    store update

---
+++ .history/src/config_20210212221602.js
@@ -0,0 +1,8 @@
+// export default {
+//     API_ENDPOINT: 'http://localhost:9090'
+//   }
+
+export default {
+  API_ENDPOINT: `postgres://xtnnikwencmgmp:222e11179dbf4409ef1913b8b6d80b038ff1edf705ded9f4405e573a05cb2a2c@ec2-52-205-3-3.compute-1.amazonaws.com:5432/d34un7jav83874`,
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
